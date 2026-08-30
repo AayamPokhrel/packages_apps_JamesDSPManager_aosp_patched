75b19f49d0673638da2f3dca6614350feba555df
AayamPokhrel <pokhrelaayam1010@gmail.com>
2026-08-30 19:46:30 +0545

    Write and track necessary files > Add binaries,config.mk from https://github.com/TogoFire/packages_apps_JamesDSPManager > Set permissions that android expects to control audio but was missing from manifest in the actual app this made denials from Android which did not let effects to be used > Tweak Android.bp to install the app as a privilaged app and add shared libs that were used from ViPER4Android.

---
+++ config.mk
@@ -0,0 +1,7 @@
+BUILD_PATH = packages/apps/JamesDSPManager
+PRODUCT_SOONG_NAMESPACES += \
+   $(BUILD_PATH)
+PRODUCT_PACKAGES += \
+	JamesDSPManager \
+	libjamesdsp \
+	privapp-permissions-james.dsp.xml
